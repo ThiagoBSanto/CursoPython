d832a8665881fcb2505c9a39611876efea873ae8
Thiago Santos <tbssantos@gmail.com>
2023-11-03 18:13:19 -0300

    Initial commit
    Signed-off-by: Thiago Santos <tbssantos@gmail.com>

---
+++ Section_2_ Python_VS_Code_Setting_Up_My_Development_Environment/aula1.py
@@ -0,0 +1,17 @@
+"""
+Docstrings são strings que 
+inserimos dentro de nosso código
+Python com o intuito de fornecer 
+uma explicação sobre o funcionamento
+deste.
+
+"""
+
+''' Usar para escrever suas notas '''
+
+# o caracterer "#" (hashtag) Permite
+#  escrever um comentário
+
+print(123)  # Na frente
+# Abaixo
+print(456)
